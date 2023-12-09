61c7731544e66a2fbd13fcb58c8a8f3d97385ab0
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2023-12-09 10:47:00 +0100

    Initial.

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+const estraverse = require('estraverse');
+const esprima = require('esprima-next');
+const escodegen = require('escodegen');
+const postcss = require('postcss');
+const postcss_minify = require('postcss-minify');
+
+const minifyLitCssTemplateLiteral = (onload_options = {}) => {
+    return {
+        name: 'Minify Lit CSS Template Literal',
+        setup(build) {
+            const postcss_processor = postcss(postcss_minify());
+
+            build.onLoad(onload_options, async args => {
+                const parse_script_result = esprima.parseScript(
+                    await fs.promises.readFile(args.path, 'utf8'), {tolerant: true}
+                );
+
+                estraverse.replace(parse_script_result, {
+                    enter: node => {
+                        if (node.type === 'TaggedTemplateExpression') {
+                            if (node.tag.name === 'css') {
+                                node.quasi.quasis = node.quasi.quasis.map(q => {
+                                    q.value = {raw: postcss_processor.process(q.value.raw).css.trim()};
+                                    return q;
+                                })
+                            } else if (node.tag.name === 'html') {
+                                node.quasi.quasis = node.quasi.quasis.map(q => {
+                                    q.value = {raw: q.value.raw.replace(/(^\s+|\s+$|\n)/gm, '').trim()};
+                                    return q;
+                                });
+                            }
+                        }
+
+                        return node;
+                    }
+                });
+
+                return {contents: escodegen.generate(parse_script_result)};
+            });
+        }
+    };
+}
+
+module.exports = {
+    minifyLitCssTemplateLiteral
+};
+
